66dcb2f8e1acd2956b905222a862cc99a9e1fe3f
Jugalcody <jugalnerist@gmail.com>
2023-10-08 00:30:58 +0530

    added operation to list,tuple

--- simjava/ds/List.java
+++ simjava/ds/List.java
@@ -8,6 +8,41 @@ short [] listShort=new short[1];
 double[] listDouble=new double[1];
 String[] listString=new String[1];
 
+
+public int[] listInt(){
+return listInt;
+}
+
+
+public short[] listShort(){
+return listShort;
+}
+
+
+
+public byte[] listByte(){
+return listByte;
+}
+
+
+
+public float[] listFloat(){
+return listFloat;
+}
+
+
+
+public double[] listDouble(){
+return listDouble;
+}
+
+
+
+public String[] listString(){
+return listString;
+}
+
+
 public int[] createIntList(int size){
 listInt=new int[size];
 Scanner sc=new Scanner(System.in);
@@ -220,7 +255,7 @@ s+=listFloat[i];
 }
 }
 s+="]";
-System.out.println(s);
+System.out.println( s);
 
 }
 
@@ -231,10 +266,8 @@ System.out.println(s);
 public int[] appendIntList(int num){
 int s=listInt.length;
 int[]copy=new int[s+1];
-int k=0;
 for(int i=0;i<s;i++){
 copy[i]=listInt[i];
-k+=1;
 }
 copy[s]=num;
 this.listInt=copy;
@@ -245,15 +278,315 @@ return listInt;
 public String[] appendStringList(String num){
 int s=listString.length;
 String[]copy=new String[s+1];
-int k=0;
 for(int i=0;i<s;i++){
 copy[i]=listString[i];
-k+=1;
 }
 copy[s]=num;
 this.listString=copy;
 return listString;
 }
 
+
+public short[] appendShortList(short num){
+
+int s=listShort.length;
+short[]copy=new short[s+1];
+for(int i=0;i<s;i++){
+copy[i]=listShort[i];
+}
+copy[s]=num;
+this.listShort=copy;
+return listShort;
+}
+
+public float[] appendFloatList(float num){
+int s=listFloat.length;
+float[]copy=new float[s+1];
+for(int i=0;i<s;i++){
+copy[i]=listFloat[i];
+}
+copy[s]=num;
+this.listFloat=copy;
+return listFloat;
+}
+
+public double[] appendDoubleList(double num){
+int s=listDouble.length;
+double[]copy=new double[s+1];
+for(int i=0;i<s;i++){
+copy[i]=listDouble[i];
+}
+copy[s]=num;
+this.listDouble=copy;
+return listDouble;
+}
+
+public byte[] appendByteList(byte num){
+int s=listByte.length;
+byte[]copy=new byte[s+1];
+for(int i=0;i<s;i++){
+copy[i]=listByte[i];
+}
+copy[s]=num;
+this.listByte=copy;
+return listByte;
+}
+
+
+//remove element
+
+public int popIntList(){
+int s=listInt.length;
+int num=listInt[s-1];
+int[]copy=new int[s-1];
+for(int i=0;i<s-1;i++){
+copy[i]=listInt[i];
+}
+
+this.listInt=copy;
+return num;
+}
+
+
+public int popShortList(){
+int s=listShort.length;
+short num=listShort[s-1];
+short[]copy=new short[s-1];
+for(int i=0;i<s-1;i++){
+copy[i]=listShort[i];
+}
+
+this.listShort=copy;
+return num;
+}
+
+
+
+
+public float popFloatList(){
+int s=listFloat.length;
+float num=listFloat[s-1];
+float[]copy=new float[s-1];
+for(int i=0;i<s-1;i++){
+copy[i]=listFloat[i];
+}
+
+this.listFloat=copy;
+return num;
+}
+
+
+public byte popByteList(){
+int s=listByte.length;
+byte num=listByte[s-1];
+byte[]copy=new byte[s-1];
+for(int i=0;i<s-1;i++){
+copy[i]=listByte[i];
+}
+
+this.listByte=copy;
+return num;
+}
+
+
+
+public double popDoubleList(){
+int s=listDouble.length;
+double num=listDouble[s-1];
+double[]copy=new double[s-1];
+for(int i=0;i<s-1;i++){
+copy[i]=listDouble[i];
+}
+
+this.listDouble=copy;
+return num;
+}
+
+
+
+public String popStringList(){
+int s=listString.length;
+String num=listString[s-1];
+String[]copy=new String[s-1];
+for(int i=0;i<s-1;i++){
+copy[i]=listString[i];
+}
+
+this.listString=copy;
+return num;
+}
+
+
+//insert into some index
+
+//int list insert method
+public void insert(int[] list,int num,int index){
+
+int[] newlist=new int[list.length+1];
+for(int i=0;i<index;i++){
+newlist[i]=list[i];
+}
+newlist[index]=num;
+for(int i=index+1;i<newlist.length;i++){
+newlist[i]=list[i-1];
+}
+this.listInt=newlist;
+}
+
+//byte list insert method
+public void insert(byte[] list,byte num,int index){
+
+byte[] newlist=new byte[list.length+1];
+for(int i=0;i<index;i++){
+newlist[i]=list[i];
+}
+newlist[index]=num;
+for(int i=index+1;i<newlist.length;i++){
+newlist[i]=list[i-1];
+}
+this.listByte=newlist;
+}
+
+// string list insert method
+
+
+
+public void insert(String[] list,String num,int index){
+
+String[] newlist=new String[list.length+1];
+for(int i=0;i<index;i++){
+newlist[i]=list[i];
+}
+newlist[index]=num;
+for(int i=index+1;i<newlist.length;i++){
+newlist[i]=list[i-1];
+}
+this.listString=newlist;
+}
+
+// short list insert method
+
+
+public void insert(short[] list,short num,int index){
+
+short[] newlist=new short[list.length+1];
+for(int i=0;i<index;i++){
+newlist[i]=list[i];
+}
+newlist[index]=num;
+for(int i=index+1;i<newlist.length;i++){
+newlist[i]=list[i-1];
+}
+this.listShort=newlist;
+}
+
+// float list insert method
+
+
+
+public void insert(float[] list,float num,int index){
+
+float[] newlist=new float[list.length+1];
+for(int i=0;i<index;i++){
+newlist[i]=list[i];
+}
+newlist[index]=num;
+for(int i=index+1;i<newlist.length;i++){
+newlist[i]=list[i-1];
+}
+this.listFloat=newlist;
+}
+
+// double list insert method
+
+public void insert(double[] list,double num,int index){
+
+double[] newlist=new double[list.length+1];
+for(int i=0;i<index;i++){
+newlist[i]=list[i];
+}
+newlist[index]=num;
+for(int i=index+1;i<newlist.length;i++){
+newlist[i]=list[i-1];
+}
+this.listDouble=newlist;
+}
+
+
+
+// remove from an index
+
+
+public void remove(int[] list,int index){
+
+for(int i=index;i<list.length-1;i++){
+
+list[i]=list[i+1];
+}
+
+list[list.length-1]=0;
+
+}
+
+
+public void remove(short[] list,int index){  //short
+
+for(int i=index;i<list.length-1;i++){
+
+list[i]=list[i+1];
+}
+
+list[list.length-1]=0;
+
+}
+
+
+
+public void remove(float[] list,int index){  //float 
+
+for(int i=index;i<list.length-1;i++){
+
+list[i]=list[i+1];
+}
+
+list[list.length-1]=0;
+
+}
+
+
+public void remove(double[] list,int index){  //double
+
+for(int i=index;i<list.length-1;i++){
+
+list[i]=list[i+1];
+}
+
+list[list.length-1]=0;
+
+}
+
+
+public void remove(byte[] list,int index){  //byte
+
+for(int i=index;i<list.length-1;i++){
+
+list[i]=list[i+1];
+}
+
+list[list.length-1]=0;
+
+}
+
+
+public void remove(String[] list,int index){  //String
+
+for(int i=index;i<list.length-1;i++){
+
+list[i]=list[i+1];
+}
+
+list[list.length-1]=null;
+
+}
 
 }
--- simjava/ds/Tuple.java
+++ simjava/ds/Tuple.java
@@ -1,15 +1,22 @@
 package simjava.ds;
 import java.util.Scanner;
 public class Tuple{
+int[] tupleInt=new int[1];
+float [] tupleFloat=new float[1];
+byte [] tupleByte=new byte[1];
+short [] tupleShort=new short[1];
+double[] tupleDouble=new double[1];
+String[] tupleString=new String[1];
+
 
 public int[] createIntTuple(int size){
-int[] arr=new int[size];
+tupleInt=new int[size];
 Scanner sc=new Scanner(System.in);
 String s="(";
 for(int i=0;i<size;i++){
 System.out.print("Enter "+(i+1)+" element : ");
 int n=sc.nextInt();
-arr[i]=n;
+tupleInt[i]=n;
 if(i!=size-1)
 s+=n+",";
 else{
@@ -18,21 +25,39 @@ s+=n;
 }
 s+=")";
 System.out.println(s);
-return arr;       
+return tupleInt;       
 }
 
 
 
+public String[] createStringTuple(int size){
+tupleString=new String[size];
+Scanner sc=new Scanner(System.in);
+String s="(";
+for(int i=0;i<size;i++){
+System.out.print("Enter "+(i+1)+" element : ");
+String n=sc.next();
+tupleString[i]=n;
+if(i!=size-1)
+s+=n+",";
+else{
+s+=n;
+}
+}
+s+=")";
+System.out.println(s);
+return tupleString;       
+}
 
 
 public float[] createFloatTuple(int size){
-float [] arr=new float[size];
+tupleFloat=new float[size];
 Scanner sc=new Scanner(System.in);
 String s="(";
 for(int i=0;i<size;i++){
 System.out.print("Enter "+(i+1)+" element : ");
 float n=sc.nextFloat();
-arr[i]=n;
+tupleFloat[i]=n;
 if(i!=size-1)
 s+=n+",";
 else{
@@ -41,18 +66,18 @@ s+=n;
 }
 s+=")";
 System.out.println(s);
-return arr;       
+return tupleFloat;       
 }
 
 
 public byte[] createByteTuple(int size){
-byte [] arr=new byte[size];
+tupleByte=new byte[size];
 Scanner sc=new Scanner(System.in);
 String s="(";
 for(int i=0;i<size;i++){
 System.out.print("Enter "+(i+1)+" element : ");
 byte n=sc.nextByte();
-arr[i]=n;
+tupleByte[i]=n;
 if(i!=size-1)
 s+=n+",";
 else{
@@ -61,7 +86,7 @@ s+=n;
 }
 s+=")";
 System.out.println(s);
-return arr;       
+return tupleByte;       
 }
 
 
@@ -70,13 +95,13 @@ return arr;
 
 
 public short[] createShortTuple(int size){
-short [] arr=new short[size];
+tupleShort=new short[size];
 Scanner sc=new Scanner(System.in);
 String s="(";
 for(int i=0;i<size;i++){
 System.out.print("Enter "+(i+1)+" element : ");
 short n=sc.nextShort();
-arr[i]=n;
+tupleShort[i]=n;
 if(i!=size-1)
 s+=n+",";
 else{
@@ -85,18 +110,18 @@ s+=n;
 }
 s+=")";
 System.out.println(s);
-return arr;       
+return tupleShort;       
 }
 
 
 public double[] createDoubleTuple(int size){
-double[] arr=new double[size];
+tupleDouble=new double[size];
 Scanner sc=new Scanner(System.in);
 String s="(";
 for(int i=0;i<size;i++){
 System.out.print("Enter "+(i+1)+" element : ");
 Double n=sc.nextDouble();
-arr[i]=n;
+tupleDouble[i]=n;
 if(i!=size-1)
 s+=n+",";
 else{
@@ -105,19 +130,19 @@ s+=n;
 }
 s+=")";
 System.out.println(s);
-return arr;       
+return tupleDouble;       
 }
 
 
 
-public void printTuple(int[] tuple){
+public void printIntTuple(){
 String s="(";
 
-for(int i=0;i<tuple.length;i++){
-if(i!=tuple.length-1)
-s+=tuple[i]+",";
+for(int i=0;i<tupleInt.length;i++){
+if(i!=tupleInt.length-1)
+s+=tupleInt[i]+",";
 else{
-s+=tuple[i];
+s+=tupleInt[i];
 }
 }
 s+=")";
@@ -125,14 +150,14 @@ System.out.println(s);
 
 }
 
-public void printTuple(String[] tuple){
+public void printStringTuple(){
 String s="(";
 
-for(int i=0;i<tuple.length;i++){
-if(i!=tuple.length-1)
-s+=tuple[i]+",";
+for(int i=0;i<tupleString.length;i++){
+if(i!=tupleString.length-1)
+s+=tupleString[i]+",";
 else{
-s+=tuple[i];
+s+=tupleString[i];
 }
 }
 s+=")";
@@ -140,14 +165,14 @@ System.out.println(s);
 
 }
 
-public void printTuple(byte[] tuple){
+public void printByteTuple(){
 String s="(";
 
-for(int i=0;i<tuple.length;i++){
-if(i!=tuple.length-1)
-s+=tuple[i]+",";
+for(int i=0;i<tupleByte.length;i++){
+if(i!=tupleByte.length-1)
+s+=tupleByte[i]+",";
 else{
-s+=tuple[i];
+s+=tupleByte[i];
 }
 }
 s+=")";
@@ -156,14 +181,14 @@ System.out.println(s);
 }
 
 
-public void printTuple(short[] tuple){
+public void printShortTuple(){
 String s="(";
 
-for(int i=0;i<tuple.length;i++){
-if(i!=tuple.length-1)
-s+=tuple[i]+",";
+for(int i=0;i<tupleShort.length;i++){
+if(i!=tupleShort.length-1)
+s+=tupleShort[i]+",";
 else{
-s+=tuple[i];
+s+=tupleShort[i];
 }
 }
 s+=")";
@@ -171,33 +196,143 @@ System.out.println(s);
 
 }
 
-public void printTuple(double[] tuple){
+public void printDoubleTuple(){
 String s="(";
 
-for(int i=0;i<tuple.length;i++){
-if(i!=tuple.length-1)
-s+=tuple[i]+",";
+for(int i=0;i<tupleDouble.length;i++){
+if(i!=tupleDouble.length-1)
+s+=tupleDouble[i]+",";
 else{
-s+=tuple[i];
+s+=tupleDouble[i];
 }
 }
 s+=")";
 System.out.println(s);
 
 }
-public void printTuple(float[] tuple){
+public void printFloatTuple(){
 String s="(";
 
-for(int i=0;i<tuple.length;i++){
-if(i!=tuple.length-1)
-s+=tuple[i]+",";
+for(int i=0;i<tupleFloat.length;i++){
+if(i!=tupleFloat.length-1)
+s+=tupleFloat[i]+",";
 else{
-s+=tuple[i];
+s+=tupleFloat[i];
 }
 }
 s+=")";
 System.out.println(s);
 
 }
+
+
+
+
+public int[] addIntTuple(Tuple tup){
+int[] tup2=tup.tupleInt;
+int s=tupleInt.length+tup2.length;
+int[]copy=new int[s];
+for(int i=0;i<tupleInt.length;i++){
+copy[i]=tupleInt[i];
+}
+for(int i=tupleInt.length;i<tup2.length+tupleInt.length;i++){
+copy[i]=tup2[i-tupleInt.length];
+
+}
+this.tupleInt=copy;
+return tupleInt;
+}
+
+
+public int[] addIntTuple(int[] tup2){
+int s=tupleInt.length+tup2.length;
+int[]copy=new int[s];
+for(int i=0;i<tupleInt.length;i++){
+copy[i]=tupleInt[i];
+}
+for(int i=tupleInt.length;i<tup2.length+tupleInt.length;i++){
+copy[i]=tup2[i-tupleInt.length];
+
+}
+this.tupleInt=copy;
+return tupleInt;
+}
+
+public String[] addStringTuple(String[] tup2){
+int s=tupleString.length+tup2.length;
+String[]copy=new String[s];
+for(int i=0;i<tupleString.length;i++){
+copy[i]=tupleString[i];
+}
+for(int i=tupleString.length;i<tup2.length+tupleString.length;i++){
+copy[i]=tup2[i-tupleString.length];
+
+}
+this.tupleString=copy;
+return tupleString;
+}
+
+
+
+public float[] addFloatTuple(float[] tup2){
+int s=tupleFloat.length+tup2.length;
+float[]copy=new float[s];
+for(int i=0;i<tupleFloat.length;i++){
+copy[i]=tupleFloat[i];
+}
+for(int i=tupleFloat.length;i<tup2.length+tupleFloat.length;i++){
+copy[i]=tup2[i-tupleFloat.length];
+
+}
+this.tupleFloat=copy;
+return tupleFloat;
+}
+
+
+public double[] addDoubleTuple(double[] tup2){
+int s=tupleFloat.length+tup2.length;
+double[]copy=new double[s];
+for(int i=0;i<tupleDouble.length;i++){
+copy[i]=tupleDouble[i];
+}
+for(int i=tupleDouble.length;i<tup2.length+tupleDouble.length;i++){
+copy[i]=tup2[i-tupleDouble.length];
+
+}
+this.tupleDouble=copy;
+return tupleDouble;
+}
+
+public short[] addShortTuple(short[] tup2){
+int s=tupleFloat.length+tup2.length;
+short[]copy=new short[s];
+for(int i=0;i<tupleShort.length;i++){
+copy[i]=tupleShort[i];
+}
+for(int i=tupleShort.length;i<tup2.length+tupleShort.length;i++){
+copy[i]=tup2[i-tupleShort.length];
+
+}
+this.tupleShort=copy;
+return tupleShort;
+}
+
+
+public byte[] addByteTuple(byte[] tup2){
+int s=tupleByte.length+tup2.length;
+byte[]copy=new byte[s];
+for(int i=0;i<tupleByte.length;i++){
+copy[i]=tupleByte[i];
+}
+for(int i=tupleByte.length;i<tup2.length+tupleByte.length;i++){
+copy[i]=tup2[i-tupleByte.length];
+
+}
+this.tupleByte=copy;
+return tupleByte;
+}
+
+
+
 
 }
